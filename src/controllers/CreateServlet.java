74a7bd6e9fed176ae341a1f1ce562ab15f09d89f
fuku33 <fukumitu3@gmail.com>
2021-08-17 17:48:39 +0900

    commit test

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -8,12 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/* commit test */
+
 @WebServlet("/create")
 public class CreateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    public CreateServlet() {
-        super();
-    }
+	public CreateServlet() {
+		super();
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	}
